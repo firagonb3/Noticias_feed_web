fcf2919742734dcd8ce45ca976173cb95a5c0100
firagonb3 <93060162+firagonb3@users.noreply.github.com>
2022-04-10 06:04:20 +0200

    Add files via upload

---
+++ firagon/categorias/cosplay.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+  <head>
+    <?php include('../recursos/php/host.php'); ?>
+  </head>
+  <body>
+    <header>
+      <!--Menu-->
+      <?php require($uri . "/recursos/index/header.html") ?>
+    </header>
+    <main>
+      
+
+        <!--Noticias-->
+        
+    </main>
+    <footer>
+      <?php require($uri . "/recursos/index/footer.php"); ?>
+    </footer>
+  </body>
+</html>
